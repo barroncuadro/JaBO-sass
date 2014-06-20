80e18b4be76a4cba8aa984848160c634f797ed5e
barroncuadro <man@barroncuadro.com>
2014-06-19 18:56:05 -0700

    lots of sass-cache stuff
    ?

--- config.rb
+++ config.rb
@@ -3,6 +3,7 @@ add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
+http_generated_images_path = "../images"
 css_dir = "stylesheets"
 sass_dir = "scss"
 images_dir = "images"
